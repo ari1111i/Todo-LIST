8036f65d48c429a34aad8e1d9a48f68758772b51
ari1111i <arietomarkulov2810@gmail.com>
2023-07-30 04:27:56 +0600

    meals-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Meals from './components/meals/Meals';
+import MealForm from './components/meals/MealForm'
+import MealItem from './components/meals/MealItem';
+import Button from './components/UI/Button'
+
+
 
 const App = () => {
   return (
-    <div>Order-Food
-      <Header/>
-      <MealsSummary/>
-      <Meals/>
-      <MealItem/>
-      <Button/>
+    <div>
+     <Meals/>
+     <MealForm/>
+     <MealItem/>
+     <Button/>
+    
     </div>
   )
 }
 
 export default App
-
 
 
 
--- src/components/UI/button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Children } from 'react'
-
-export const Button = ({children, onClick, disabled, variant, size, ...restProps}) => {
-  return  <button onClick={onClick} disabled={disabled} {...restProps}>{children}</button>
-
-}
-
